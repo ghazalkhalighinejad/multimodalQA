2a4b15a4d22dae4d55624313d9f813131f19bdb5
shang-zhu <shangzhu@andrew.cmu.edu>
2024-05-25 23:55:26 -0400

    fix multi-choice dataloader bug; add more models for Llava benchmark

--- api/llava.py
+++ api/llava.py
@@ -6,18 +6,40 @@ import pdb
 import warnings
 warnings.filterwarnings("ignore")
 
-llava_pipeline = pipeline(
-    model = "llava-hf/llava-1.5-13b-hf",
-    device_map="auto",
-)
 transformers.utils.logging.set_verbosity(transformers.logging.ERROR)
-def call_llava(text_query, image_path, temperature=0.0, extra_explanation=True):
+def call_llava(text_query, image_path, model, temperature=0.0, extra_explanation=True):
     """
     Args:
         text_query: str. The query text.
         image_path: str. The path to the image.
         temperature: float. The temperature for the generation.
     """
+    if model=='llava-7b':
+        llava_pipeline = pipeline(
+            model = "llava-hf/llava-1.5-7b-hf",
+            device_map="auto",
+        )
+    elif model=='llava-13b':
+        llava_pipeline = pipeline(
+            model = "llava-hf/llava-1.5-13b-hf",
+            device_map="auto",
+        )
+    elif model=='llava-34b':
+        llava_pipeline = pipeline(
+            model = "liuhaotian/llava-v1.6-34b",
+            device_map="auto",
+        )
+    elif model=='llava-sci':
+        llava_pipeline = pipeline(
+            model = "liuhaotian/llava-lcs558k-scienceqa-vicuna-13b-v1.3",
+            device_map="auto",
+        )
+    elif model=='llava-mistral':
+        llava_pipeline = pipeline(
+            model = "llava-hf/llava-v1.6-mistral-7b-hf",
+            device_map="auto",
+        )        
+
     img = Image.open(image_path)
     prompt = "<image>\nUSER: " + text_query + "\nASSISTANT:"
     response = llava_pipeline(prompt=prompt, images=img, max_new_tokens=1024)[0]['generated_text']
@@ -34,6 +56,6 @@ def call_llava(text_query, image_path, temperature=0.0, extra_explanation=True):
 
 if __name__ == "__main__":
     text_query = "What is in the image?"
-    image_path = "../graph/0_1_2_yes.png"
-    pdb.set_trace()
+    image_path = "../images/mol_caption_0.png"
+    # pdb.set_trace()
     print(call_llava(text_query, image_path))
--- eval.py
+++ eval.py
@@ -5,24 +5,26 @@ import pandas as pd
 import json 
 from tqdm import tqdm
 import argparse
+import ast
 
-from api.gpt4 import (
-    call_gpt4_vision, 
-    call_gpt4, 
-    call_gpt3_5
-)
+from api.llava import call_llava
+# from api.gpt4 import (
+#     call_gpt4_vision, 
+#     call_gpt4, 
+#     call_gpt3_5
+# )
 
-from api.gemini import (
-    call_gemini_pro_vision, 
-    call_gemini_pro,
-    call_palm_2
-)
+# from api.gemini import (
+#     call_gemini_pro_vision, 
+#     call_gemini_pro,
+#     call_palm_2
+# )
 
-from api.claude3 import (
-    call_claude3_vision,
-    call_claude3
-)
-from api.together_api import call_together
+# from api.claude3 import (
+#     call_claude3_vision,
+#     call_claude3
+# )
+# from api.together_api import call_together
 
 
 def call_vision(text_query, image_path, model):
@@ -30,8 +32,10 @@ def call_vision(text_query, image_path, model):
         response = call_gemini_pro_vision(text_query, image_path)
     elif model == "gpt4":
         response = call_gpt4_vision(text_query, image_path)
-    elif model == "llava":
-        response = call_llava(text_query, image_path)
+    elif "llava" in model:
+        response = call_llava(text_query, image_path, model, extra_explanation=False)
+    # elif model == "llava-13b":
+    #     response = call_llava(text_query, image_path, extra_explanation=False)
     elif model == "claude3":
         response = call_claude3_vision(text_query, image_path)
     else:
@@ -59,40 +63,50 @@ def call_text(text_query, system_content, model):
 def prepare_vision_prompt(dataset):
 
     prompts = []
+    if not os.path.exists("images"):
+        os.makedirs("images")
 
     for row in dataset:
-        
-        id, question, choices, label, description = row["id"], row["question"], row["choices"], row["label"], row["description"]
+        id, question, choices, label, description, img = row["id"], row["question"], row["choices"], row["label"], row["description"], row['image']
+        try:
 
-        prompt = f"""Given the image of a chemical compound, answer the following multiple choice question.
+            # Convert the string to a list
+            choices = ast.literal_eval(choices)
+            assert len(choices)==4, "length of choices should be 4"
 
-        Question: {question}
-        Choices: 
-        1. {choices[0]}
-        2. {choices[1]}
-        3. {choices[2]}
-        4. {choices[3]}
+            prompt = f"""Given the image of a chemical compound, answer the following multiple choice question.
 
-        ONLY return a number from 1 to 4. DO NOT write the answer.
-        """
-        id = row["id"].split("/")[-1]
-        image = f"images/{id}.png"
+            Question: {question}
+            Choices: 
+            1. {choices[0]}
+            2. {choices[1]}
+            3. {choices[2]}
+            4. {choices[3]}
 
-        prompts.append({
-            "id": id,
-            "text_query": prompt,
-            "image_path": image,
-            "true_label": label
-        })
+            ONLY return a number from 1 to 4. DO NOT write the answer.
+            """
+            id = row["id"].split("/")[-1]
+            image = f"images/{id}.png"
+
+            #save iamges only when it's first loaded
+            # img.save(image, 'png')
+
+            prompts.append({
+                "id": id,
+                "text_query": prompt,
+                "image_path": image,
+                "true_label": label
+            })
+        except:
+            print('Error:', id)
 
     return prompts
 
 
 def parse_answer(response):
 
-    response = ''.join(filter(str.isdigit, response))
+    response = ''.join(filter(str.isdigit, response)) 
     response = int(response)
-
     return response
 
 
@@ -109,9 +123,6 @@ if __name__ == '__main__':
     parser.add_argument('--together', action="store_true", help="use together api")
     parser.add_argument('--resume', action="store_true", help="resume from last checkpoint")
     args = parser.parse_args()
-
-    if args.model == "llava":
-        from api.llava import call_llava
    
     result_dir = "chemistry_benchmark"
 
@@ -130,17 +141,15 @@ if __name__ == '__main__':
     num_rows: 1198
     })
     """
-    dataset = load_dataset("shangzhu/scibench", split = "valid")
+    dataset = load_dataset("shangzhu/ChemQA-lite", split = "test")
     # change the ids of dataset to have only what comes after the last '/'
     dataset = dataset.map(lambda x: {"id": x["id"].split("/")[-1], "question": x["question"], "choices": x["choices"], "label": x["label"], "description": x["description"]})
 
-    results = pd.read_json("chemistry_benchmark/gemini_results.json")
-    dataset = dataset.filter(lambda x: x["id"] in results["question_id"].values)        
+    # results = pd.read_json("chemistry_benchmark/gemini_results.json")
+    # dataset = dataset.filter(lambda x: x["id"] in results["question_id"].values)        
 
 
     prompts = prepare_vision_prompt(dataset)
-
-
     
     df_prompts = pd.DataFrame(prompts)
 
@@ -155,44 +164,41 @@ if __name__ == '__main__':
 
         if args.resume:
 
-            if row["id"] in df_resume["question_id"].values:
-                df_resume_row = df_resume[df_resume["question_id"] == row["id"]].iloc[0]
-
-                df_results = df_results.append({
-                    "question_id": df_resume_row["question_id"],
-                    "response": {
-                        "vision": df_resume_row["response"]["vision"]
-                    },
-                    "correct": {
-                        "vision": check_correctness(df_resume_row["response"]["vision"], true_label)
-                    },
-                    "true_label": true_label
-                }, ignore_index=True)
-                    
+            if row["id"] in df_resume["id"].values:
+                df_resume_row = df_resume[df_resume["id"] == row["id"]].iloc[0]
+                print('df_resume:', df_resume_row)
+                df_=pd.DataFrame({
+                    "id": df_resume_row["id"],
+                    "response": df_resume_row["response"],
+                    "correct": df_resume_row["correct"],
+                    "true_label": df_resume_row["true_label"]
+                })
+                df_results = pd.concat([df_results,df_]) #, ignore_index=True
                 continue
                     
-        if args.model in ['gemini', 'gpt4', 'llava', 'claude3']:
-            vision_response = call_vision(prompt, image_path, args.model)
-        else:
-            vision_response = ""
-
-        df_results = df_results.append({
-            "question_id": row["id"],
-            "response": {
-                "vision": vision_response
-            },
-            "correct": {
-                "vision": check_correctness(vision_response, true_label),
-            },
+        # if args.model in ['gemini', 'gpt4', 'llava', 'llava-13b', 'llava-sci', 'claude3']:
+        # try:
+        vision_response = call_vision(prompt, image_path, args.model)
+        # except:
+        #     print('Error:', image_path)
+        #     vision_response=""
+        # else:
+        #     vision_response = ""
+        
+        df_=pd.DataFrame({
+            "id": row["id"],
+            "response": vision_response,
+            "correct": check_correctness(vision_response, true_label),
             "true_label": true_label
-        }, ignore_index=True)
+        }, index=[0])
+        df_results = pd.concat([df_results,df_]) #, ignore_index=True
 
         df_results.to_json(result_path, orient="records", indent=4)
 
     
     # get final score
     print(df_results["correct"])
-    vision_accuracy = [x["vision"] for x in df_results["correct"]].count(True) / len(df_results)
+    vision_accuracy = [x for x in df_results["correct"]].count(True) / len(df_results)
 
     # save in results dir
     with open(f"{result_dir}/{args.model}_accuracy.txt", "w") as f:
